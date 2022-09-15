2d9bc29e087352b41a545e9902be327c6d85456b
FabioBassani <avilar.fabio@yahoo.com.br>
2022-09-15 17:24:00 +0200

    project finish

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let isGoingRight = false
     let leftTimerId 
     let rightTimerId 
+    let score = 0
 
     function createDoodler() {
         grid.appendChild(doodler)
@@ -53,6 +54,16 @@ document.addEventListener('DOMContentLoaded', () => {
                 platform.bottom -= 4
                 let visual = platform.visual
                 visual.style.bottom = platform.bottom + 'px'
+
+                if (platform.bottom < 10) {
+                    let firstPlatform = platforms[0].visual
+                    firstPlatform.classList.remove('platform')
+                    platforms.shift()
+                    score++
+                    console.log(platforms)
+                    let newPlatform = new Platform(600)
+                    platforms.push(newPlatform)
+                }
             })
         }
     }
@@ -98,8 +109,14 @@ document.addEventListener('DOMContentLoaded', () => {
     function gameOver() {
         console.log('game over')
         isGameOver = true
+        while (grid.firstChild) {
+            grid.removeChild(grid.firstChild)
+        }
+        grid.innerHTML = score
         clearInterval(upTimerId)
         clearInterval(downTimerId)
+        clearInterval(leftTimerId)
+        clearInterval(rightTimerId)
     }
 
     function control(e) {
@@ -108,7 +125,7 @@ document.addEventListener('DOMContentLoaded', () => {
         } else if (e.key === 'ArrowRight') {
             moveRight()
         } else if (e.key === 'ArrowUp') {
-            //moveStraight
+            moveStraight()
         }
     }
 
@@ -124,7 +141,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 doodler.style.left = doodlerLeftSpace + 'px'
             } else moveRight()
 
-        }, 30)
+        }, 20)
     }
 
     function moveRight() {
@@ -138,7 +155,14 @@ document.addEventListener('DOMContentLoaded', () => {
                 doodlerLeftSpace += 5
                 doodler.style.left = doodlerLeftSpace + 'px'
             } else moveLeft()
-        }, 30)
+        }, 20)
+    }
+
+    function moveStraight() {
+        isGoingRight = false
+        isGoingLeft = false
+        clearInterval(rightTimerId)
+        clearInterval(leftTimerId)
     }
 
     function start() {
